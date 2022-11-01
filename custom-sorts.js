8ed467f4e4cdd74c4336c34e6eab34e5c02abb31
Sophie Main <sophiekmain@gmail.com>
2022-11-01 15:26:05 -0500

    problem 02

--- custom-sorts.js
+++ custom-sorts.js
@@ -8,7 +8,18 @@ function ageSort(users) {
 };
 
 function oddEvenSort(arr) {
-  // Your code here
+  arr.sort((a,b)=>{
+    if ((a % 2 === 0) && (b % 2 === 1)){
+        return 1;
+    }
+    if ((a % 2 === 1) && (b % 2 === 0)){
+       return -1;
+
+    } else {
+      return a-b
+    }
+  })
+  return arr
 }
 
 function validAnagrams(s, t) {
